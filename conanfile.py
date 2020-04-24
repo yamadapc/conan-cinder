657c313a5c352bde49a10a75cbfeebec963527af
yamadapc <tacla.yamada@gmail.com>
2020-04-24 18:45:48 +1000

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,64 @@
+from conans import ConanFile, CMake, tools
+
+
+class CinderConan(ConanFile):
+    name = "cinder"
+    version = "0.9.2"
+    license = """
+Copyright (c) 2010, The Cinder Project
+
+This code is intended to be used with the Cinder C++ library, http://libcinder.org
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and
+the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+    """
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake"
+    requires = [
+        'boost/1.71.0',
+    ]
+
+    def source(self):
+        self.run("git clone -b v0.9.2 https://github.com/cinder/Cinder")
+        tools.replace_in_file("Cinder/CMakeLists.txt", "project( cinder )",
+                              '''project( cinder )
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()''')
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="./Cinder")
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s'
+        #          % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="hello")
+        self.copy("*cinder.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["cinder"]
+
---
+++ test_package/example.cpp
@@ -0,0 +1,59 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/Camera.h"
+#include "cinder/Timeline.h"
+#include "cinder/MotionManager.h"
+#include "cinder/Log.h"
+
+using namespace ci;
+using namespace ci::app;
+
+class MotionBasicApp : public App {
+public:
+    virtual void	setup();
+    virtual void	update();
+    virtual void	draw();
+
+    mat4			mModelMatrix;
+    CameraPersp		mCam;
+    Anim<Color>		mBackgroundColor;
+};
+
+void MotionBasicApp::setup()
+{
+    CI_LOG_V( "gyro available: " << MotionManager::isGyroAvailable() );
+
+    MotionManager::enable( 60.0f/*, MotionManager::SensorMode::Accelerometer*/ );
+
+    mCam.setPerspective( 60, getWindowAspectRatio(), 1, 1000 );
+    mCam.lookAt( vec3( 0, 0, 3 ), vec3( 0 ) );
+}
+
+void MotionBasicApp::update()
+{
+    if( ! MotionManager::isEnabled() )
+        return;
+
+    mModelMatrix = inverse( MotionManager::getRotationMatrix() );
+
+    if( MotionManager::isShaking( 1.5f ) ) {
+        CI_LOG_V( "isShaking!" );
+        mBackgroundColor = Color::gray( MotionManager::getShakeDelta() / 10.0f );
+        timeline().apply( &mBackgroundColor, Color::black(), 0.5f, EaseInQuad() );
+    }
+}
+
+void MotionBasicApp::draw()
+{
+    gl::clear( mBackgroundColor() );
+    gl::enableDepthRead();
+
+    gl::setMatrices( mCam );
+    gl::multModelMatrix( mModelMatrix );
+
+    gl::drawCoordinateFrame();
+//	gl::drawColorCube( vec3::zero(), vec3( 1, 1, 1 ) );
+}
+
+CINDER_APP( MotionBasicApp, RendererGl )
+
